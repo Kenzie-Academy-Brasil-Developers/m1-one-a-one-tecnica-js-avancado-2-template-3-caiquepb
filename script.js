ae0e9cacf3d92a7c31c911c6c4360bc29c89ddd9
Caique Batista <caiquepbatistaa@gmail.com>
2022-11-03 13:44:32 -0300

    entrega realizada

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//Desenvolva seu código aqui!
+function verificaLista(lista, numero){
+
+    let cont = 0
+
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i] == numero){
+            cont++
+        }
+    }
+
+    if(cont % 5 == 0){
+        return "É múltiplo de 5"
+    }else{return "Não é múltiplo de 5"}
+
+}
